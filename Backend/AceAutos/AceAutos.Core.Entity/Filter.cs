e94eb2234fa0e7eb47f152e624352d0f948c1f39
abdi2148 <abdiqafar.abas@gmail.com>
2020-11-27 09:47:47 +0100

    Classes added

---
+++ Backend/AceAutos/AceAutos.Core.Entity/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AceAutos.Core.Entity
+{
+    class Filter
+    {
+        public string OrderDirection { get; set; }
+        public string OrderProperty { get; set; }
+        public string SearchText { get; set; }
+        public string SearchField { get; set; }
+        public int ItemsPrPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Backend/AceAutos/AceAutos.Core/DomainService/ICarService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AceAutos.Core.DomainService
+{
+    interface ICarService
+    {
+    }
+}
