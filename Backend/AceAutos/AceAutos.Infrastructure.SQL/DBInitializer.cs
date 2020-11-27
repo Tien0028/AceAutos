91f582ac7307dfe2635d57cddc565d156f6e1115
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-11-27 11:46:07 +0100

    some things

--- Backend/AceAutos/AceAutos.Infrastructure.SQL/DBInitializer.cs
+++ Backend/AceAutos/AceAutos.Infrastructure.SQL/DBInitializer.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using PetShopApplicationSolution.Infrastructure.Data.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AceAutos.Infrastructure.SQL
 {
-    class DBInitializer
+    public class DBInitializer : IDBInitializer
     {
+        private readonly IAuthenticationHelper authenticationHelper;
+
+        public DBInitializer(IAuthenticationHelper authHelper)
+        {
+            authenticationHelper = authHelper;
+        }
+
+        public void SeedDB(DBContext atx)
+        {
+            // Delete the database, if it already exists. I do this because an
+            // existing database may not be compatible with the entity model,
+            // if the entity model was changed since the database was created.
+            // This operation has no effect for an in-memory database.
+            atx.Database.EnsureDeleted();
+
+            // Create the database, if it does not already exists. This operation
+            // has no effect for an in-memory database.
+            atx.Database.EnsureCreated();
+        }
     }
 }
