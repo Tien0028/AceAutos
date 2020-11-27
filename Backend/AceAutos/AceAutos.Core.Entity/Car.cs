17e606f8eda45cb89edaa564a8b06941e84012ba
Domex99 <rave.monkey36@gmail.com>
2020-11-27 12:23:31 +0100

    car attributes

--- Backend/AceAutos/AceAutos.Core.Entity/Car.cs
+++ Backend/AceAutos/AceAutos.Core.Entity/Car.cs
@@ -6,5 +6,13 @@ namespace AceAutos.Core.Entity
 {
     public class Car
     {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public string Fuel { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
     }
 }
