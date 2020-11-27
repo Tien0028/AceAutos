5e52b9f4e523b8e01a7071fc984f63299d1661d1
Domex99 <rave.monkey36@gmail.com>
2020-11-27 12:26:02 +0100

    Revert "car attributes"
    This reverts commit 17e606f8eda45cb89edaa564a8b06941e84012ba.

--- Backend/AceAutos/AceAutos.Core.Entity/Filter.cs
+++ Backend/AceAutos/AceAutos.Core.Entity/Filter.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace AceAutos.Core.Entity
 {
-    public class Filter
+    class Filter
     {
-       
+        public string OrderDirection { get; set; }
+        public string OrderProperty { get; set; }
+        public string SearchText { get; set; }
+        public string SearchField { get; set; }
         public int ItemsPrPage { get; set; }
         public int CurrentPage { get; set; }
-       
+        public int Price { get; set; }
     }
 }
