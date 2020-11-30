486ab08507d59f57b61cabbe54cf92b50ca7c460
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-11-30 15:00:28 +0100

    update and start unittest

---
+++ Backend/AceAutos/UnitTests2/CarTest.cs
@@ -0,0 +1,19 @@
+using AceAutos.Core.DomainService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Xunit;
+
+namespace UnitTests2
+{
+    [TestClass]
+    public class CarTest
+    {
+        [Fact]
+        public void ReadById()
+        {
+            //Validate Data
+            Mock<ICarRepository> carRepo = new Mock<ICarRepository>();
+
+        }
+    }
+}
