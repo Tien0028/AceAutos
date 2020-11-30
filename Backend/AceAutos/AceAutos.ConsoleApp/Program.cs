b77e8fc6875ad8dcbc593883c333c0cfe654da99
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-11-30 09:36:28 +0100

    Changes

---
+++ Backend/AceAutos/AceAutos.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AceAutos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
