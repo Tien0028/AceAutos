a71c010933aaacb3e0e2f710823b66e9e74f48df
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2020-11-30 15:30:44 +0100

    Try this

--- Backend/AceAutos/AceAutos.RestAPI/WebAPI/Program.cs
+++ Backend/AceAutos/AceAutos.RestAPI/WebAPI/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,11 +17,8 @@ namespace WebAPI
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>();
     }
 }
