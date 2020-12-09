a359e62ec216460633cb9d6f285c856dc69c1281
abdi2148 <abdiqafar.abas@gmail.com>
2020-12-09 16:46:09 +0100

    Customer changes/Order added

--- Backend/AceAutos/AceAutos.Core.Entity/Customer.cs
+++ Backend/AceAutos/AceAutos.Core.Entity/Customer.cs
@@ -6,8 +6,12 @@ namespace AceAutos.Core.Entity
 {
     public class Customer
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Order> AllOrders { get; set; }
+
     }
 }
---
+++ Backend/AceAutos/AceAutos.Core.Entity/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AceAutos.Core.Entity
+{
+    public class Order
+    {
+        public int ID { get; set; }
+    }
+}
