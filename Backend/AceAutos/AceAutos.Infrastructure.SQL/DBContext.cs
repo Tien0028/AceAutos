4b39cf5cccfb98becd83e85954583d517defc11d
abdi2148 <abdiqafar.abas@gmail.com>
2020-12-09 16:47:31 +0100

    DbContext/aceauto.db updated

--- Backend/AceAutos/AceAutos.Infrastructure.SQL/DBContext.cs
+++ Backend/AceAutos/AceAutos.Infrastructure.SQL/DBContext.cs
@@ -16,5 +16,7 @@ namespace AceAutos.Infrastructure.SQL
         public DbSet<Car> Cars { get; set; }
         public DbSet<CarType> CarTypes { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
     }
 }
